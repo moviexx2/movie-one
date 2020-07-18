05d38072aeff30ec88dce706fd7b2af7dbb4c2e4
agusTrih <ant3dong@gmail.com>
2020-07-18 16:07:54 +0700

    add schema

---
+++ models/movie.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const MovieSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    years: {
+        type: Number,
+        required
+    },
+    UserID: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+    } createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+})
