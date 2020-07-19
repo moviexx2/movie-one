5249ad09d421d38d81c3557241e4919be21a99e5
agusTrih <ant3dong@gmail.com>
2020-07-19 15:25:35 +0700

    add delete and try edit

--- routes/products/controller.js
+++ routes/products/controller.js
@@ -1,5 +1,6 @@
 const { hashed } = require("../../helpers");
 const { Product } = require("../../models");
+const Movie = require("../../models/Movie");
 
 module.exports = {
     create: async (req, res) => {
@@ -27,6 +28,24 @@ module.exports = {
             const result = await Product.find({ UserID }).populate("UserID");
 
             res.send({ message: "Add Product successfull", data: result });
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    deleteUserId: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const result = await Movie.findByIdAndDelete(id);
+            res.send({ message: "Delete succes", data: result });
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    editUserID: async (req, res) => {
+        try {
+            const { UserID } = req.params;
+
+            const result = await Movie.find({ UserID }).populate("UserID");
         } catch (error) {
             console.log(error);
         }
