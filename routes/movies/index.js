dc05aedd9ed14c40f4ffc37f47e9aca319addd63
agusTrih <ant3dong@gmail.com>
2020-07-19 14:03:47 +0700

    add router in index.js

--- routes/movies/index.js
+++ routes/movies/index.js
@@ -2,3 +2,7 @@ const router = require("express").Router();
 const { forwardAuthenticate, ensureAuthenticate } = require("../../config");
 
 router.post("/", require("./controller").create);
+router.get("/", require("./controller").get);
+router.get("/findByUserID/:UserID", require("./controller").getByUserID);
+
+module.exports = router;
