f65bd2d7c48ef46a88a6913aa9ee0db4530399fb
agusTrih <ant3dong@gmail.com>
2020-07-19 21:04:04 +0700

    rombak tahap edit

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(expressLayout);
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.redirect("http://localhost:4005/users/register");
+    res.send({ message: "Welcome to my API with passport" });
 });
 app.use("/users", require("./routes/users"));
 // app.use("/products", require("./routes/products"));
--- routes/movies/index.js
+++ routes/movies/index.js
@@ -1,12 +1,16 @@
 const router = require("express").Router();
 const { forwardAuthenticate, ensureAuthenticate } = require("../../config");
 
-router.post("/", require("./controller").create);
-router.get("/", require("./controller").get);
+// testing 1
+
+router.get("/", ensureAuthenticate, require("./controller").get);
 router.get("/findByUserID/:UserID", require("./controller").getByUserID);
+router.post("/", require("./controller").create);
+router.put("/:id", require("./controller").editMovieID);
+router.delete("/:id", require("./controller").deleteMovieId);
 // testing
-router.put("/edit/:UserID", require("./controller").editUserID);
-router.get("/", forwardAuthenticate, require("./controller").home);
-router.get("/:id", forwardAuthenticate, require("./controller").deleteUserId);
+// router.put("/edit/:UserID", require("./controller").editUserID);
+// router.get("/", forwardAuthenticate, require("./controller").home);
+// router.get("/:id", forwardAuthenticate, require("./controller").deleteUserId);
 
 module.exports = router;
